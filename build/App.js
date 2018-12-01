3bf7c6b59d46ff09f3ad59e35182b342233cee0f
EheAha <3486183988@qq.com>
2018-12-01 17:43:57 +0800

    first commit

---
+++ build/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Home from './pages/home/Home';
+export default class App extends React.Component {
+    render() {
+        return (React.createElement(Home, { name: 'shanshan' }));
+    }
+}
+//# sourceMappingURL=App.js.map
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import Home from './pages/home/Home'
+
+export default class App extends React.Component{
+    render(){
+        return(
+            <Home name={'shanshan'}></Home>
+        )
+    }
+}
